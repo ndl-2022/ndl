e443d03cbfad7c21adef4e8604b887328408f1a2
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2022-12-02 11:53:19 +0100

    feat(tower,enemy): enable CORS
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- apps/enemy/src/main.ts
+++ apps/enemy/src/main.ts
@@ -11,8 +11,15 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3334;
+  app.setGlobalPrefix(globalPrefix);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
